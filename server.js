bc8db83e56c3ca1b63ffdba820cda1d695e49053
Michał Szajner <michalszajner@wp.pl>
2023-03-20 09:03:43 +0100

    Updating logic

--- server.js
+++ server.js
@@ -24,29 +24,32 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/dodaj', (req, res) => {
-   res.sendFile(__dirname+"/dodaj.html");
+    res.sendFile(__dirname + "/dodaj.html");
 });
 
-app.get('/aktualizacja', (req, res) => {
-   res.sendFile(__dirname+"/aktualizacja.html");
+app.get('/aktualizacja/:id', async (req, res) => {
+    const id = new mongo.ObjectId(req.params.id);
+    const pracownik = await pracownicy.findOne({ "_id": id });
+    console.log(pracownik);
+    res.render("aktualizacja.ejs", { pracownik });
 });
 
 app.get('/usuwanie', (req, res) => {
-   res.sendFile(__dirname+"/usuwanie.html");
+    res.sendFile(__dirname + "/usuwanie.html");
 });
 
 app.post('/dodaj', (req, res) => {
     console.log(req);
     const { id, im, naz, st } = req.body;
     pracownicy.insertOne({
-        id: Number(`${id}`),
-        imie: `${im}`,
-        nazwisko: `${naz}`,
-        stanowisko: `${st}`,
+        id: Number(`${ id }`),
+        imie: `${ im }`,
+        nazwisko: `${ naz }`,
+        stanowisko: `${ st }`,
     });
     res.redirect('/wyswietl');
 });
@@ -54,6 +57,21 @@ app.post('/dodaj', (req, res) => {
 app.get('/wyswietl', async (req, res) => {
     let dane = await pracownicy.find().toArray();
     res.render("index.ejs", { a: dane });
+});
+
+app.post("/aktualizuj/:id", async (req, res) => {
+    const _id = new mongo.ObjectId(req.params.id);
+
+    const { id, im, naz, st } = req.body;
+    pracownicy.updateOne({ _id }, {
+        $set: {
+            imie: `${ im }`,
+            nazwisko: `${ naz }`,
+            stanowisko: `${ st }`,
+        }
+    });
+
+    res.redirect('/wyswietl');
 });
 
 app.listen(port, () => {
